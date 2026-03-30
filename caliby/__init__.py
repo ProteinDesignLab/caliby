f64b07834430df3ca03fdb24811044760595730f
Richard <richardshuai98@gmail.com>
2026-03-29 20:38:40 -0700

    Add Python API for programmatic usage

---
+++ caliby/__init__.py
@@ -0,0 +1,23 @@
+from caliby.api import (
+    CalibyModel,
+    caliby_ensemble_sample,
+    caliby_sample,
+    caliby_score,
+    caliby_score_ensemble,
+    caliby_sidechain_pack,
+    generate_ensembles,
+    load_model,
+    make_constraints,
+)
+
+__all__ = [
+    "CalibyModel",
+    "caliby_ensemble_sample",
+    "caliby_sample",
+    "caliby_score",
+    "caliby_score_ensemble",
+    "caliby_sidechain_pack",
+    "generate_ensembles",
+    "load_model",
+    "make_constraints",
+]
